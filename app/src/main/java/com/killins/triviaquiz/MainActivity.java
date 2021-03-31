37694e31d60d5082954edef47ff822c756bb882d
bradkillins <bradkillins@gmail.com>
2021-03-31 17:05:53 -0600

    Initial commit

---
+++ app/src/main/java/com/killins/triviaquiz/MainActivity.java
@@ -0,0 +1,61 @@
+package com.killins.triviaquiz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.btnStartGame);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Start game!!!!");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final int aboutMenuId = R.id.menu_about;
+        final int helpMenuId = R.id.menu_help;
+
+        switch (item.getItemId()) {
+            case aboutMenuId:
+                showToast("About Menu Item Clicked");
+                break;
+            case helpMenuId:
+                showToast("Help Menu Item Clicked");
+                break;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void showToast(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+}
---
+++ app/src/main/java/com/killins/triviaquiz/QuizQuestion.java
@@ -0,0 +1,15 @@
+package com.killins.triviaquiz;
+
+public class QuizQuestion {
+    int imageId, correctQ;
+    String q1, q2, q3, q4;
+
+    public QuizQuestion(int imageId, int correctQ, String q1, String q2, String q3, String q4){
+        this.imageId = imageId;
+        this.correctQ = correctQ;
+        this.q1 = q1;
+        this.q2 = q2;
+        this.q3 = q3;
+        this.q4 = q4;
+    }
+}
